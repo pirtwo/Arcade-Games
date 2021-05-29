3c7e5cc68390eebc65e28347e6b5298d0c41eb8c
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2021-05-29 19:56:54 +0430

    update movment logic

--- Asteroids/src/Spacecraft.cpp
+++ Asteroids/src/Spacecraft.cpp
@@ -6,15 +6,20 @@ const float PI = 3.14;
 
 Spacecraft::Spacecraft(sf::Texture &texture)
 {
+    turnRate = 4.5;
+    maxSpeed = 4.0;
+
     _velocity =
         sf::Vector2f(0, 0);
     _acceleration =
-        sf::Vector2f(1, 1);
+        sf::Vector2f(0, 0);
     _friction =
         sf::Vector2f(1, 1);
-    _turnSpeed = 0.f;
 
     _sp = sf::Sprite(texture);
+    _sp.setOrigin(sf::Vector2f(
+        _sp.getGlobalBounds().width / 2,
+        _sp.getGlobalBounds().height / 2));
 }
 
 Spacecraft::~Spacecraft()
@@ -24,37 +29,49 @@ Spacecraft::~Spacecraft()
 
 void Spacecraft::forward()
 {
-    _velocity.x = cos(getRotation() * PI / 180);
-    _velocity.y = sin(getRotation() * PI / 180);
-    _acceleration.x = 1.2;
-    _acceleration.y = 1.2;
-    _friction.x = 1.f;
-    _friction.y = 1.f;
+    _acceleration.x = 0.2;
+    _acceleration.y = 0.2;
+    _friction.x = 1;
+    _friction.y = 1;
 }
 
 void Spacecraft::stop()
 {
-    _acceleration.x = 1.f;
-    _acceleration.y = 1.f;
-    _friction.x = 0.2;
-    _friction.y = 0.2;
+    _acceleration.x = 0;
+    _acceleration.y = 0;
+    _friction.x = 0.99;
+    _friction.y = 0.99;
 }
 
 void Spacecraft::turnLeft()
 {
-    _turnSpeed = 5.f;
+    rotate(turnRate * -1);
 }
 
 void Spacecraft::turnRight()
 {
-    _turnSpeed = -5.f;
+    rotate(turnRate);
 }
 
 void Spacecraft::update()
 {
-    _velocity.x *= _acceleration.x * _friction.x;
-    _velocity.y *= _acceleration.y * _friction.y;
-    rotate(_turnSpeed);
+    // calculate magnitude of the ship
+    float m = sqrt(pow(_velocity.x, 2) + pow(_velocity.y, 2));
+
+    // control ship max speed
+    m = m >= maxSpeed ? maxSpeed : m;
+
+    // apply ship rotation
+    _velocity.x = m * cos(getRotation() * PI / 180);
+    _velocity.y = m * sin(getRotation() * PI / 180);
+
+    // apply acceleration and friction
+    _velocity.x += _velocity.x > 0 ? _acceleration.x : -_acceleration.x;
+    _velocity.y += _velocity.y > 0 ? _acceleration.y : -_acceleration.y;
+    _velocity.x *= _friction.x;
+    _velocity.y *= _friction.y;
+
+    // move the ship
     move(_velocity.x, _velocity.y);
 }
 

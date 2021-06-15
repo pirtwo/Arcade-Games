4d315222989c3fb7da59a677f22c275c015456e4
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2021-06-15 20:22:54 +0430

    add body, add bounce

--- Pong/include/Ball.h
+++ Pong/include/Ball.h
@@ -3,19 +3,24 @@
 
 #include <SFML/Graphics.hpp>
 
-class Ball : public sf::Drawable, sf::Transformable
+class Ball : public sf::Drawable, public sf::Transformable
 {
 protected:
+    float _speed;
+    float _angle;
     sf::RectangleShape _sp;
 
 public:
-    float speed;
-    float angle;
     sf::Vector2f velocity;
 
 public:
     Ball(float w, float h, sf::Color color);
     ~Ball();
+    sf::FloatRect getBody();
+    void bounceX();
+    void bounceY();
+    void setSpeed(float value);
+    void setAngle(float value);
     void update();
     void draw(sf::RenderTarget &target, sf::RenderStates states) const;
 };
--- Pong/src/Ball.cpp
+++ Pong/src/Ball.cpp
@@ -5,8 +5,8 @@
 
 Ball::Ball(float w, float h, sf::Color color)
 {
-    speed = 0;
-    angle = 0;
+    _speed = 0;
+    _angle = 0;
     _sp = sf::RectangleShape(sf::Vector2f(w, h));
     _sp.setFillColor(color);
 }
@@ -16,10 +16,41 @@ Ball::~Ball()
     //
 }
 
+sf::FloatRect Ball::getBody()
+{
+    return sf::FloatRect(
+        getPosition().x,
+        getPosition().y,
+        _sp.getGlobalBounds().width,
+        _sp.getGlobalBounds().height);
+}
+
+void Ball::bounceX()
+{
+    velocity.x *= -1;
+}
+
+void Ball::bounceY()
+{
+    velocity.y *= -1;
+}
+
+void Ball::setSpeed(float value)
+{
+    _speed = value;
+    velocity.x = cos(_angle) * _speed;
+    velocity.y = sin(_angle) * _speed;
+}
+
+void Ball::setAngle(float value)
+{
+    _angle = value * PI / 180;
+    velocity.x = cos(_angle) * _speed;
+    velocity.y = sin(_angle) * _speed;
+}
+
 void Ball::update()
 {
-    velocity.x = cos(angle) * speed;
-    velocity.y = sin(angle) * speed;
     move(velocity.x, velocity.y);
 }
 

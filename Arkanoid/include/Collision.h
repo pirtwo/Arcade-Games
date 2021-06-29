72469688e0de9164727885e8d7198b821d97dc1a
Ahmad.Fz <ahmad.f.zadeh@icloud.com>
2021-06-29 13:07:29 +0430

    add ray casting collision

--- Arkanoid/include/Collision.h
+++ Arkanoid/include/Collision.h
@@ -13,4 +13,16 @@ bool AABB(
     float bWidth,
     float bHeight);
 
+std::vector<sf::Vector2f> rcGetCollision(
+    sf::Vector2f rayA,
+    sf::Vector2f rayB,
+    sf::Vector2f boxPos,
+    sf::Vector2f boxDim);
+
+std::pair<bool, sf::Vector2f> lineSegCollision(
+    sf::Vector2f a,
+    sf::Vector2f b,
+    sf::Vector2f c,
+    sf::Vector2f d);
+
 #endif
--- Arkanoid/src/Collision.cpp
+++ Arkanoid/src/Collision.cpp
@@ -16,3 +16,62 @@ bool AABB(
            ((ay < by && by < ay + aHeight) ||
             (ay > by && ay < by + bHeight));
 }
+
+std::vector<sf::Vector2f> rcGetCollision(
+    sf::Vector2f rayA,
+    sf::Vector2f rayB,
+    sf::Vector2f boxPos,
+    sf::Vector2f boxDim)
+{
+    std::vector<sf::Vector2f> collisions;
+    std::pair<bool, sf::Vector2f> res;
+
+    res = lineSegCollision(
+        rayA, rayB,
+        sf::Vector2f(boxPos.x, boxPos.y),
+        sf::Vector2f(boxPos.x + boxDim.x, boxPos.y));
+    if (res.first)
+        collisions.push_back(res.second);
+
+    res = lineSegCollision(
+        rayA, rayB,
+        sf::Vector2f(boxPos.x, boxPos.y),
+        sf::Vector2f(boxPos.x, boxPos.y + boxDim.y));
+    if (res.first)
+        collisions.push_back(res.second);
+
+    res = lineSegCollision(
+        rayA, rayB,
+        sf::Vector2f(boxPos.x + boxDim.x, boxPos.y),
+        sf::Vector2f(boxPos.x + boxDim.x, boxPos.y + boxDim.y));
+    if (res.first)
+        collisions.push_back(res.second);
+
+    res = lineSegCollision(
+        rayA, rayB,
+        sf::Vector2f(boxPos.x, boxPos.y + boxDim.y),
+        sf::Vector2f(boxPos.x + boxDim.x, boxPos.y + boxDim.y));
+    if (res.first)
+        collisions.push_back(res.second);
+
+    return collisions;
+}
+
+std::pair<bool, sf::Vector2f> lineSegCollision(
+    sf::Vector2f a,
+    sf::Vector2f b,
+    sf::Vector2f c,
+    sf::Vector2f d)
+{
+    float t = ((a.x - c.x) * (c.y - d.y) - (a.y - c.y) * (c.x - d.x)) /
+              ((a.x - b.x) * (c.y - d.y) - (a.y - b.y) * (c.x - d.x));
+
+    float u = ((b.x - a.x) * (a.y - c.y) - (b.y - a.y) * (a.x - c.x)) /
+              ((a.x - b.x) * (c.y - d.y) - (a.y - b.y) * (c.x - d.x));
+
+    return std::pair<bool, sf::Vector2f>{
+        t >= 0 && t <= 1 && u >= 0 && u <= 1,
+        (t >= 0 && t <= 1)
+            ? sf::Vector2f{a.x + t * (b.x - a.x), a.y + t * (b.y - a.y)}
+            : sf::Vector2f{c.x + u * (d.x - c.x), c.y + u * (d.y - c.y)}};
+}

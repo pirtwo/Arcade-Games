0a4643f33f131a88cf6efaa9b49200f87ef3475d
Ahmad.Fz <ahmad.f.zadeh@icloud.com>
2021-07-11 13:31:07 +0430

    remove some code

--- Arkanoid/src/main.cpp
+++ Arkanoid/src/main.cpp
@@ -13,19 +13,6 @@ int main()
 
     Key arrowL, arrowR;
 
-    std::map<std::string, std::shared_ptr<sf::Texture>> textures;
-    textures["paddle"] = std::make_shared<sf::Texture>();
-
-    if (!textures["paddle"]->loadFromFile("./assets/paddle.png"))
-    {
-        return EXIT_FAILURE;
-    }
-
-    auto paddle = std::make_shared<Paddle>(*textures["paddle"]);
-    paddle->setSpeed(1.2);
-    paddle->setScale(0.2, 0.2);
-    paddle->setPosition(50, 450);
-
     while (window.isOpen())
     {
         sf::Event e;
@@ -65,20 +52,10 @@ int main()
             }
         }
 
-        //======= update =========//
-        if (arrowL.isDown)
-            paddle->slideLeft();
-        if (arrowR.isDown)
-            paddle->slideRight();
-        if (!arrowL.isDown && !arrowR.isDown)
-            paddle->stop();
-
-        paddle->update(1.0);
+        //======== update =========//
 
         //======== draw ==========//
         window.clear();
-
-        window.draw(*paddle);
 
         window.display();
     }

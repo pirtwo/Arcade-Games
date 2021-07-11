cf781fcac9ee38ef0d5c7b1226c48d9a0571225a
Ahmad.Fz <ahmad.f.zadeh@icloud.com>
2021-07-11 13:30:47 +0430

    modify some classes

--- Arkanoid/include/Entity.h
+++ Arkanoid/include/Entity.h
@@ -8,6 +8,8 @@ class Entity : public sf::Drawable, public sf::Transformable
 public:
     std::string name;
     virtual ~Entity() {}
+    virtual sf::Vector2f getVelocity() = 0;
+    virtual sf::FloatRect getBounds() = 0;
     virtual void update() = 0;
     virtual void draw(
         sf::RenderTarget &target,
--- Arkanoid/include/Paddle.h
+++ Arkanoid/include/Paddle.h
@@ -5,9 +5,17 @@
 
 class Paddle : public Entity
 {
+protected:
+    float _speed;
+    sf::Sprite _sp;
+    sf::Vector2f _vel;
+
 public:
-    Paddle();
+    Paddle(sf::Texture &t, float speed);
     ~Paddle();
+    sf::FloatRect getBounds() override;
+    void moveLeft();
+    void moveRight();
     void update() override;
     void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
 };

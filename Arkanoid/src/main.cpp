7637b3ff8b99eb40ef298331177e2178c38d7a16
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2021-07-13 13:13:53 +0430

    add tiles and effects

--- Arkanoid/src/main.cpp
+++ Arkanoid/src/main.cpp
@@ -4,33 +4,93 @@
 #include <memory>
 #include "Key.h"
 #include "Ball.h"
+#include "Tile.h"
 #include "Entity.h"
 #include "Paddle.h"
 #include "Particle.h"
 #include "Collision.h"
+
+/**
+ * TODO: create different levels with different tile layout
+ * TODO: store level layouts in a file
+ */
+
+void createTiles(
+    std::vector<std::shared_ptr<Entity>> &world,
+    sf::Texture &t,
+    float x,
+    float y,
+    int rows,
+    int cols,
+    float scaleX,
+    float scaleY,
+    float paddingX,
+    float paddingY)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            auto tile = std::make_shared<Tile>(t, 1);
+            tile->setScale(scaleX, scaleY);
+            tile->setPosition(
+                x + j * (tile->getBounds().width + paddingX),
+                y + i * (tile->getBounds().height + paddingY));
+            world.push_back(tile);
+        }
+    }
+}
+
+void addEffect(
+    std::vector<std::shared_ptr<ParticleSystem>> &effects,
+    const std::vector<std::shared_ptr<sf::Texture>> &textures,
+    int n,
+    float x,
+    float y)
+{
+    auto p = std::make_shared<ParticleSystem>(
+        textures, 1, 0, 360, 2.f, 3.f, 0.05, 0.09, 0, 360, 3, 4, false);
+    p->setPosition(x, y);
+    p->addFule(n);
+    effects.push_back(p);
+}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(500, 700), "Arkanoid");
     window.setFramerateLimit(60);
 
-    Key arrowL, arrowR;
-
-    std::vector<std::shared_ptr<Entity>> world;
+    // ========= load assets =======//
 
     std::map<std::string, std::shared_ptr<sf::Texture>> textures;
     textures["ball"] =
         std::make_shared<sf::Texture>(sf::Texture());
     textures["paddle"] =
         std::make_shared<sf::Texture>(sf::Texture());
-    textures["tile_green"] =
+    textures["tileGreen_01"] =
+        std::make_shared<sf::Texture>(sf::Texture());
+    textures["tileGreen_02"] =
         std::make_shared<sf::Texture>(sf::Texture());
     if (!textures["ball"]->loadFromFile("./assets/ball.png") ||
         !textures["paddle"]->loadFromFile("./assets/paddle.png") ||
-        !textures["tile_green"]->loadFromFile("./assets/tile_green.png"))
+        !textures["tileGreen_01"]->loadFromFile("./assets/tileGreen_01.png") ||
+        !textures["tileGreen_02"]->loadFromFile("./assets/tileGreen_02.png"))
     {
         return EXIT_FAILURE;
     }
+
+    //======== initialize =========//
+
+    Key arrowL, arrowR;
+
+    sf::Vector2f cp, cn;
+    sf::FloatRect bound;
+
+    std::vector<std::shared_ptr<Entity>> world;
+
+    std::vector<std::shared_ptr<ParticleSystem>> effects;
+    std::vector<std::shared_ptr<sf::Texture>> effectTextures;
+    effectTextures.push_back(textures["tileGreen_02"]);
 
     auto paddle = std::make_shared<Paddle>(*textures["paddle"], 5.5);
     paddle->setScale(0.08, 0.08);
@@ -38,7 +98,7 @@ int main()
     world.push_back(paddle);
 
     auto ball = std::make_shared<Ball>(*textures["ball"]);
-    ball->vel = sf::Vector2f(-1, -1);
+    ball->vel = sf::Vector2f(-2.5, -2.5);
     ball->setScale(0.1, 0.1);
     ball->setPosition(100, 100);
     world.push_back(ball);
@@ -47,8 +107,13 @@ int main()
         window.getSize().x, window.getSize().y));
     fieldBounds.setPosition(0, 0);
 
-    sf::Vector2f cp, cn;
-    sf::FloatRect bound;
+    createTiles(
+        world,
+        *textures["tileGreen_01"],
+        50, 50,
+        5, 6,
+        0.2, 0.2,
+        20, 20);
 
     while (window.isOpen())
     {
@@ -99,9 +164,11 @@ int main()
 
         // collision detection
 
-        for (auto &&i : world)
+        for (auto i = world.begin(); i != world.end(); i++)
         {
-            if (i->name == "ball")
+            auto e = i->get();
+
+            if (e->name == "ball")
             {
                 // check for walls collision
                 if (rayVsBox(
@@ -121,30 +188,64 @@ int main()
                 }
             }
 
-            if (i->name == "paddle")
+            if (e->name == "paddle")
             {
-                // check for ball hit to paddle
+                // check for ball collision to paddle
 
-                bound = i->getBounds();
+                bound = e->getBounds();
+                bound.top -= ball->getBounds().height / 2;
+                bound.left -= ball->getBounds().width / 2;
+                bound.width += ball->getBounds().width;
+                bound.height += ball->getBounds().height;
 
                 if (rayVsBox(
                         ball->getPosition(),
                         ball->vel,
-                        sf::Vector2f(bound.top, bound.left),
+                        sf::Vector2f(bound.left, bound.top),
                         sf::Vector2f(bound.width, bound.height),
                         cp, cn))
                 {
-                    ball->setPosition(cp);
-                    if (cn.x == 1 || cn.x == -1)
+                    if (cn.x != 0 && cn.y != 0)
+                        ball->vel *= -1.f;
+                    else if (cn.x != 0)
                         ball->vel.x *= -1;
-                    if (cn.y == 1 || cn.y == -1)
+                    else if (cn.y != 0)
                         ball->vel.y *= -1;
+                    ball->setPosition(cp + cn);
                 }
             }
 
-            if (i->name == "tile")
+            if (e->name == "tile")
             {
-                // check for ball hit
+                // check for ball collision to tile
+
+                bound = e->getBounds();
+                bound.top -= ball->getBounds().height / 2;
+                bound.left -= ball->getBounds().width / 2;
+                bound.width += ball->getBounds().width;
+                bound.height += ball->getBounds().height;
+
+                if (rayVsBox(
+                        ball->getPosition(),
+                        ball->vel,
+                        sf::Vector2f(bound.left, bound.top),
+                        sf::Vector2f(bound.width, bound.height),
+                        cp, cn))
+                {
+                    if (cn.x != 0 && cn.y != 0)
+                        ball->vel *= -1.f;
+                    else if (cn.x != 0)
+                        ball->vel.x *= -1;
+                    else if (cn.y != 0)
+                        ball->vel.y *= -1;
+                    ball->setPosition(cp + cn);
+
+                    addEffect(effects, effectTextures, 5, cp.x, cp.y);
+
+                    i = world.erase(i);
+                    if (i == world.end())
+                        break;
+                }
             }
         }
 
@@ -153,9 +254,18 @@ int main()
             i->update();
         }
 
+        for (auto &&i : effects)
+        {
+            i->update(1.f);
+        }
+
         //======== draw ========//
         window.clear();
         for (auto &&i : world)
+        {
+            window.draw(*i);
+        }
+        for (auto &&i : effects)
         {
             window.draw(*i);
         }

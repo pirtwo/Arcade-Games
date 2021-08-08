e99d1f9b9095df5428b02b3cf59ada7d2f564426
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2021-08-08 10:59:30 +0430

    add game loop

--- SpaceInvaders/src/main.cpp
+++ SpaceInvaders/src/main.cpp
@@ -1,8 +1,32 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    /* code here */
+    sf::RenderWindow window(sf::VideoMode(500, 700), "Space Invaders");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        sf::Event e;
+        while (window.pollEvent(e))
+        {
+            switch (e.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        //====== update =====//
+
+        //====== draw =======//
+        window.clear();
+        window.display();
+    }
 
     return EXIT_SUCCESS;
 }

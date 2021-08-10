d6b17097f9325107ab4e42799c1e00e7bdea4b2b
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2021-08-10 12:07:26 +0430

    add some logic

--- SpaceInvaders/src/main.cpp
+++ SpaceInvaders/src/main.cpp
@@ -1,9 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <memory>
+#include <vector>
+#include "Atlas.h"
+#include "Loader.h"
+#include "Entity.h"
+#include "Alien.h"
+#include "Helper.h"
+
+struct State
+{
+    int lastAlienMS = 0;
+} state;
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(500, 700), "Space Invaders");
     window.setFramerateLimit(60);
+
+    //====== load assets ======//
+    auto atlas = std::make_shared<Atlas>();
+    if (!atlas->texture.loadFromFile("./assets/texture.png") ||
+        !loadAtlasData("./assets/texture.data", atlas->data))
+    {
+        return EXIT_FAILURE;
+    }
+
+    //====== initialize ======//
+    sf::Clock clock;
+    int elapsedMS = 0;
+
+    std::vector<std::shared_ptr<Entity>> world;
+
+    auto spawnAlien = [&]()
+    {
+        char key[50];
+        snprintf(key, 50, "alien_%02d.png", randInt(1, 8));
+
+        auto elm = std::make_shared<Alien>(key, *atlas);
+        elm->owner = "cpu";
+        elm->hp = 1;
+        elm->vel.y = randFloat(1, 2);
+        elm->setScale(0.2, 0.2);
+        elm->setPosition(randFloat(0, window.getSize().x - 50), 0);
+        world.push_back(elm);
+    };
 
     while (window.isOpen())
     {
@@ -23,8 +63,23 @@ int main()
 
         //====== update =====//
 
+        elapsedMS = clock.getElapsedTime().asMilliseconds();
+
+        if (elapsedMS - state.lastAlienMS > 500)
+        {
+            spawnAlien();
+            state.lastAlienMS = elapsedMS;
+        }
+
+        for (auto &&i : world)
+            i->update();
+
         //====== draw =======//
         window.clear();
+
+        for (auto &&i : world)
+            window.draw(*i);
+
         window.display();
     }
 

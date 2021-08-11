05f09e29e78bd2f5ea7978889928809c8240176f
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2021-08-11 13:26:11 +0430

    add cannon and projectile spawn

--- SpaceInvaders/src/main.cpp
+++ SpaceInvaders/src/main.cpp
@@ -1,15 +1,19 @@
 #include <SFML/Graphics.hpp>
 #include <memory>
 #include <vector>
+#include "Key.h"
 #include "Atlas.h"
+#include "Helper.h"
 #include "Loader.h"
 #include "Entity.h"
+#include "Cannon.h"
 #include "Alien.h"
-#include "Helper.h"
+#include "Projectile.h"
 
 struct State
 {
     int lastAlienMS = 0;
+    int lastFireMS = 0;
 } state;
 
 int main()
@@ -29,7 +33,22 @@ int main()
     sf::Clock clock;
     int elapsedMS = 0;
 
+    sf::FloatRect bounds(
+        sf::Vector2f(0, 0),
+        sf::Vector2f(window.getSize().x, window.getSize().y));
+
+    Key leftArrow, rightArrow, spacebar;
+
     std::vector<std::shared_ptr<Entity>> world;
+
+    auto cannon = std::make_shared<Cannon>("cannon.png", *atlas);
+    cannon->owner = "player";
+    cannon->speed = 3.5;
+    cannon->setScale(0.2, 0.2);
+    cannon->setPosition(
+        window.getSize().x / 2,
+        window.getSize().y - 50);
+    world.push_back(cannon);
 
     auto spawnAlien = [&]()
     {
@@ -41,7 +60,20 @@ int main()
         elm->hp = 1;
         elm->vel.y = randFloat(1, 2);
         elm->setScale(0.2, 0.2);
-        elm->setPosition(randFloat(0, window.getSize().x - 50), 0);
+        elm->setPosition(randFloat(50, window.getSize().x - 50), 0);
+        world.push_back(elm);
+    };
+
+    auto spawnProjectile = [&](std::string owner, sf::Vector2f pos)
+    {
+        auto elm =
+            owner == "player"
+                ? std::make_shared<Projectile>("cannon_projectile.png", *atlas)
+                : std::make_shared<Projectile>("alien_projectile.png", *atlas);
+        elm->owner = owner;
+        elm->vel.y = owner == "player" ? -2 : 2;
+        elm->scale(0.2, 0.2);
+        elm->setPosition(pos);
         world.push_back(elm);
     };
 
@@ -52,10 +84,25 @@ int main()
         {
             switch (e.type)
             {
+            case sf::Event::KeyPressed:
+                if (e.key.code == sf::Keyboard::Key::Left)
+                    leftArrow.onPress();
+                if (e.key.code == sf::Keyboard::Key::Right)
+                    rightArrow.onPress();
+                if (e.key.code == sf::Keyboard::Key::Space)
+                    spacebar.onPress();
+                break;
+            case sf::Event::KeyReleased:
+                if (e.key.code == sf::Keyboard::Key::Left)
+                    leftArrow.onRelease();
+                if (e.key.code == sf::Keyboard::Key::Right)
+                    rightArrow.onRelease();
+                if (e.key.code == sf::Keyboard::Key::Space)
+                    spacebar.onRelease();
+                break;
             case sf::Event::Closed:
                 window.close();
                 break;
-
             default:
                 break;
             }
@@ -65,6 +112,19 @@ int main()
 
         elapsedMS = clock.getElapsedTime().asMilliseconds();
 
+        if (leftArrow.isDown)
+            cannon->moveLeft();
+        if (rightArrow.isDown)
+            cannon->moveRight();
+        if (!leftArrow.isDown && !rightArrow.isDown)
+            cannon->stop();
+
+        if (spacebar.isDown && elapsedMS - state.lastFireMS > 300)
+        {
+            spawnProjectile("player", cannon->getPosition());
+            state.lastFireMS = elapsedMS;
+        }
+
         if (elapsedMS - state.lastAlienMS > 500)
         {
             spawnAlien();
@@ -73,6 +133,16 @@ int main()
 
         for (auto &&i : world)
             i->update();
+
+        for (auto i = world.begin(); i != world.end(); i++)
+        {
+            if (i->get()->hasDispose() || !bounds.contains(i->get()->getPosition()))
+            {
+                i = world.erase(i);
+                if (i == world.end())
+                    break;
+            }
+        }
 
         //====== draw =======//
         window.clear();

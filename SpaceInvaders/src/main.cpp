ade613053bf454b20ce7a23bdc8f02a25ffe3b01
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2021-08-14 13:13:49 +0430

    rename some vars, add blast animation

--- SpaceInvaders/src/main.cpp
+++ SpaceInvaders/src/main.cpp
@@ -9,11 +9,14 @@
 #include "Cannon.h"
 #include "Alien.h"
 #include "Projectile.h"
+#include "Animation.h"
 
 struct State
 {
-    int lastAlienMS = 0;
-    int lastFireMS = 0;
+    int score = 0;
+    int lives = 0;
+    int lastAlienSpawnMS = 0;
+    int lastCannonFireMS = 0;
 } state;
 
 int main()
@@ -40,6 +43,7 @@ int main()
     Key leftArrow, rightArrow, spacebar;
 
     std::vector<std::shared_ptr<Entity>> world;
+    std::vector<std::shared_ptr<Animation>> anims;
 
     auto cannon = std::make_shared<Cannon>("cannon.png", *atlas);
     cannon->owner = "player";
@@ -58,7 +62,7 @@ int main()
         auto elm = std::make_shared<Alien>(key, *atlas);
         elm->owner = "cpu";
         elm->hp = 1;
-        elm->vel.y = randFloat(1, 2);
+        elm->vel.y = 1.2;
         elm->setScale(0.2, 0.2);
         elm->setPosition(randFloat(50, window.getSize().x - 50), 0);
         world.push_back(elm);
@@ -72,9 +76,27 @@ int main()
                 : std::make_shared<Projectile>("alien_projectile.png", *atlas);
         elm->owner = owner;
         elm->vel.y = owner == "player" ? -2 : 2;
+        elm->hp = 1;
         elm->scale(0.2, 0.2);
         elm->setPosition(pos);
         world.push_back(elm);
+    };
+
+    auto spawnBlast = [&](sf::Vector2f pos)
+    {
+        std::list<std::string> frameList =
+            {"blast_01.png",
+             "blast_02.png",
+             "blast_03.png",
+             "blast_04.png",
+             "blast_05.png",
+             "blast_06.png"};
+
+        auto elm = std::make_shared<Animation>(*atlas, frameList);
+        elm->setSpeed(60);
+        elm->setScale(0.2, 0.2);
+        elm->setPosition(pos);
+        anims.push_back(elm);
     };
 
     while (window.isOpen())
@@ -119,27 +141,51 @@ int main()
         if (!leftArrow.isDown && !rightArrow.isDown)
             cannon->stop();
 
-        if (spacebar.isDown && elapsedMS - state.lastFireMS > 300)
+        if (spacebar.isDown && elapsedMS - state.lastCannonFireMS > 300)
         {
             spawnProjectile("player", cannon->getPosition());
-            state.lastFireMS = elapsedMS;
+            state.lastCannonFireMS = elapsedMS;
         }
 
-        if (elapsedMS - state.lastAlienMS > 500)
+        if (elapsedMS - state.lastAlienSpawnMS > 3000)
         {
             spawnAlien();
-            state.lastAlienMS = elapsedMS;
+            state.lastAlienSpawnMS = elapsedMS;
         }
 
         for (auto &&i : world)
             i->update();
 
+        for (auto &&i : anims)
+            i->update(1000 / 60);
+
+        for (auto &&i : world)
+        {
+            for (auto &&j : world)
+                if (i != j && i->getBounds().intersects(j->getBounds()))
+                    i->collisions.push_back(j);
+        }
+
+        for (auto &&i : world)
+            i->handleCollisions();
+
         for (auto i = world.begin(); i != world.end(); i++)
         {
             if (i->get()->hasDispose() || !bounds.contains(i->get()->getPosition()))
             {
+                spawnBlast(i->get()->getPosition());
                 i = world.erase(i);
                 if (i == world.end())
+                    break;
+            }
+        }
+
+        for (auto i = anims.begin(); i != anims.end(); i++)
+        {
+            if (i->get()->completed())
+            {
+                i = anims.erase(i);
+                if (i == anims.end())
                     break;
             }
         }
@@ -148,6 +194,9 @@ int main()
         window.clear();
 
         for (auto &&i : world)
+            window.draw(*i);
+
+        for (auto &&i : anims)
             window.draw(*i);
 
         window.display();
